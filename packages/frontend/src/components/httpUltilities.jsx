e7504185e296c7808be5cc26bdbf7987d82e280c
DylanNguyen99 <71548549+DylanNguyen99@users.noreply.github.com>
2024-12-02 21:25:44 -0800

    change http link

--- packages/frontend/src/components/httpUltilities.jsx
+++ packages/frontend/src/components/httpUltilities.jsx
@@ -1,4 +1,4 @@
-const URL = "https://csc307-cjbaece4hebag3hj.westus3-01.azurewebsites.net"
+const URL = "https://main-taskarena-drg5e6gfc2bjake9.westus3-01.azurewebsites.net/"
 //const URL = "http://localhost:8001"
 function FetchPostUser(account) {
     const promise = fetch(`${URL}/signup`, {
