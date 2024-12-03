2a2442c5df42ec5c91d074002a1ef5eefabcb933
DylanNguyen99 <71548549+DylanNguyen99@users.noreply.github.com>
2024-12-02 22:46:11 -0800

    modify http url

--- packages/frontend/src/components/httpUltilities.jsx
+++ packages/frontend/src/components/httpUltilities.jsx
@@ -1,4 +1,4 @@
-const URL = "https://main-taskarena-drg5e6gfc2bjake9.westus3-01.azurewebsites.net/"
+const URL = "https://backend-task-arena-bhaxftapffehhhcj.westus3-01.azurewebsites.net"
 //const URL = "http://localhost:8001"
 function FetchPostUser(account) {
     const promise = fetch(`${URL}/signup`, {
